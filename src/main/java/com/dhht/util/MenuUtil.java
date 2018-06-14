024fa8439490a7722577234eef7e40fb70ea3910
ShredderMing <eternallovext@gmail.com>
2018-06-14 17:00:06 +0000

    动态菜单

---
+++ src/main/java/com/dhht/util/MenuUtil.java
@@ -0,0 +1,30 @@
+package com.dhht.util;
+
+import com.dhht.model.Menus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuUtil {
+    /**
+     *
+     * @param menus 所有是菜单的资源
+     * @return 处理数据返回前端需要的部分
+     */
+    public static List<Map> genMenu(List<Menus> menus) {
+        if (menus == null) return null;
+        List<Map> r = new ArrayList<>();
+        for (Menus menu : menus) {
+            Map m = new HashMap();
+            m.put("name", menu.getName());
+            m.put("icon", menu.getIcon());
+            String[] paths = menu.getPath().split("/");
+            m.put("path", paths[paths.length - 1]);
+            m.put("children", genMenu(menu.getChildren()));
+            r.add(m);
+        }
+        return r;
+    }
+}
