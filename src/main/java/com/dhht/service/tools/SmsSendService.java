486086d5a8cc08c26b59cf716fe7214c38fcf3b2
zhaoxinglong <317666291@qq.com>
2018-06-25 09:17:49 +0800

    添加短信发送Service类。 引入了测试类。 增加配置参数注入bean的方法。 精简了pom文件中的重复配置项。

---
+++ src/main/java/com/dhht/service/tools/SmsSendService.java
@@ -0,0 +1,31 @@
+package com.dhht.service.tools;
+
+import java.util.ArrayList;
+
+/**
+ * 短信发送服务接口
+ *
+ * @author 赵兴龙
+ * @date 2018.6.22
+ */
+public interface SmsSendService {
+
+    /**
+     * 密码重置验证码模板ID
+     */
+    int TEMPLATE_CODE_RESET_PASSWORD = 63278;
+
+    /**
+     * 中国短信号码地区代号
+     */
+    String NATION_CODE_CHINA = "86";
+
+    /**
+     * 通过模板发送短信给单个手机号
+     * @param phoneNumber 手机号码
+     * @param templateId 模板代号
+     * @param params 模板中需要的查询，数量与模板中定义的一致
+     * @return true：发送成功、false：发送失败
+     */
+    boolean sendSingleMsgByTemplate(String phoneNumber, int templateId, ArrayList<String> params);
+}
