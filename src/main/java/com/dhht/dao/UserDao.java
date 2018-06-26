52d79828e8e6bdba1d78d4a8fcf37563cda97aa9
cy <1010985950@qq.com>
2018-06-26 08:59:50 +0800

    6.26 用户管理代码分离和前端交互修改1

--- src/main/java/com/dhht/dao/UserDao.java
+++ src/main/java/com/dhht/dao/UserDao.java
@@ -22,7 +22,7 @@ public interface UserDao {
 
     List<User> findAllSuser();
 
-    List<User> find(@Param("realName")String realName, @Param("roleId")String roleId, @Param("districtId")String districtId);
+    List<User> find(@Param("realName")String realName,@Param("districtId")String districtId, @Param("roleId")String roleId);
 
     User findByNo(@Param("id") String id);
 
