fa8c727b6b374cd9aac6f42e628daf2c1fc68928
cuiyang <1010985950@qq.com>
2018-07-18 11:54:49 +0800

    7.18  seal查询修改2

--- src/main/java/com/dhht/service/seal/Impl/SealServiceImpl.java
+++ src/main/java/com/dhht/service/seal/Impl/SealServiceImpl.java
@@ -193,8 +193,22 @@ public class SealServiceImpl implements SealService {
             seal.setRecordDepartmentCode(recordCode);
             list = sealDao.selectByCodeAndName(seal);
         }else if(status.equals("00")){
-
             list = sealDao.selectUndelivered(seal);
+        }else if(status.equals("04")){
+            seal.setIsRecord(true);
+            list = sealDao.selectByCodeAndName(seal);
+        }else if(status.equals("05")){
+            seal.setIsRecord(true);
+            seal.setIsMake(true);
+            seal.setIsDeliver(true);
+            seal.setIsLoss(true);
+            list = sealDao.selectByCodeAndName(seal);
+        }else if (status.equals("06")){
+            seal.setIsRecord(true);
+            seal.setIsMake(true);
+            seal.setIsDeliver(true); 
+            seal.setIsLogout(true);
+            list = sealDao.selectByCodeAndName(seal);
         }
 
         PageInfo<Seal> result = new PageInfo<>(list);
