8d07dc5e6d20b28871b659dfaf2b43973c5230cb
cuiyang <1010985950@qq.com>
2018-07-20 16:02:47 +0800

    7.20  印章统计3

--- src/main/java/com/dhht/service/seal/SealCuontService.java
+++ src/main/java/com/dhht/service/seal/SealCuontService.java
@@ -6,6 +6,7 @@ import com.dhht.model.SealCount;
 import java.util.List;
 
 public interface SealCuontService {
+     
     //按照刻制企业
     List<SealCount> countByDepartment(List<String> makeDepartmentCodes, String districtId, List<String> sealTypeCodes, List<String> Status, String startTime, String endTime);
 
