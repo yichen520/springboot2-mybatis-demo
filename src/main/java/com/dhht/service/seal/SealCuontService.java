cfd5ddcbd57eb46630d383ccd2a52103a99e54c0
cuiyang <1010985950@qq.com>
2018-07-20 15:54:30 +0800

    7.20  印章统计2

--- src/main/java/com/dhht/service/seal/SealCuontService.java
+++ src/main/java/com/dhht/service/seal/SealCuontService.java
@@ -9,5 +9,5 @@ public interface SealCuontService {
     //按照刻制企业
     List<SealCount> countByDepartment(List<String> makeDepartmentCodes, String districtId, List<String> sealTypeCodes, List<String> Status, String startTime, String endTime);
 
-    List<SealCount> countByDistrictId(List<String> districtIds,List<String> sealTypeCodes, List<String> Status, String startTime, String endTime );
+//    List<SealCount> countByDistrictId(List<String> districtIds,List<String> sealTypeCodes, List<String> Status, String startTime, String endTime );
 }
