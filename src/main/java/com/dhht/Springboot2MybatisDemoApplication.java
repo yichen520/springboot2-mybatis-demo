836a0a12c23e0c184abfa114287fe76d24180dbb
cuiyang <1010985950@qq.com>
2018-07-25 16:32:05 +0800

    7.25  redis

--- src/main/java/com/dhht/Springboot2MybatisDemoApplication.java
+++ src/main/java/com/dhht/Springboot2MybatisDemoApplication.java
@@ -4,14 +4,16 @@ package com.dhht;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @MapperScan("com.dhht.dao")
-public class Springboot2MybatisDemoApplication  {
+public class Springboot2MybatisDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2MybatisDemoApplication.class, args);
@@ -25,7 +27,10 @@ public class Springboot2MybatisDemoApplication  {
 //			}
 //		};
 //	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Springboot2MybatisDemoApplication.class);
+	}
 	//设置单文件不能大于10M
 	@Bean
 	public MultipartConfigElement multipartConfigElement() {
