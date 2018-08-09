e57042248290b14e8365e49a79682299b5486ba7
cuiyang <1010985950@qq.com>
2018-08-09 18:13:15 +0800

    Merge branch 'master' of 10.0.0.27:seal_source_v2 into 6.6cuiy
    # Conflicts:
    #	src/main/java/com/dhht/service/employee/Impl/EmployeeServiceImp.java

--- src/main/java/com/dhht/service/user/UserService.java
+++ src/main/java/com/dhht/service/user/UserService.java
@@ -16,21 +16,22 @@ import java.util.Map;
  */
 public interface UserService  {
 
-    int delete(String id);
 
-    PageInfo<User> find(User user,String realName,String roleId,String districtId,int pageNum, int pageSize);
+    int delete(String id); //用户通过Id删除
 
-    PageInfo<User> selectByDistrict(String id,int pageSum,int pageNum);
+    PageInfo<User> find(User user,String realName,String roleId,String districtId,int pageNum, int pageSize); //按照用户名称,角色,区域id进行查询
 
-    int deleteByUserName(String roleId, String telphone);
+    PageInfo<User> selectByDistrict(String id,int pageSum,int pageNum);// 根据区域查找用户
 
-    User findByUserName(String userName);
+    int deleteByUserName(String roleId, String telphone);  //根据用户账号删除用户,用于其他模块调用
 
-    int insert(String telphone,String roleId,String realName,String districtId);
+    User findByUserName(String userName); //根据用户账号查找用户
 
-    int update(String oldTelPhone,String telPhone,String roleId,String realName,String districtId);
+    int insert(String telphone,String roleId,String realName,String districtId); //添加用户 输入项为手机号,角色id,用户名称,区域id
 
-    User findById(String Id);
+    int update(String oldTelPhone,String telPhone,String roleId,String realName,String districtId);  //更新用户  输入项为老手机号,新手机号码,角色id,用户名,区域id
+
+    User findById(String Id);//更加id查找user
 
 }
 
