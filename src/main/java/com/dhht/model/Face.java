7e5657686771456a3d6e6d322149acbcac153ad0
XuZhengPing <591979486@qq.com>
2018-08-14 17:10:01 +0800

    优化

--- src/main/java/com/dhht/model/Face.java
+++ src/main/java/com/dhht/model/Face.java
@@ -1,5 +1,16 @@
 package com.dhht.model;
 
+import lombok.Data;
+
+@Data
 public class Face {
-    
+    private String fileAURl;
+
+    private String fileBURL;
+
+    private float num;
+
+    private String maeeage;
+
+    private String isPass;
 }
