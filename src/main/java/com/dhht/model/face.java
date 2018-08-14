1ce7aade460fcda954e301011070039cdc4a8678
cuiyang <1010985950@qq.com>
2018-08-14 10:58:38 +0800

    Merge branch 'master' of 10.0.0.27:seal_source_v2 into 6.6cuiy
    # Conflicts:
    #	src/main/java/com/dhht/service/employee/Impl/EmployeeServiceImp.java

---
+++ src/main/java/com/dhht/model/face.java
@@ -0,0 +1,20 @@
+package com.dhht.model;
+
+import lombok.Data;
+
+/**
+ * Created by imac_dhht on 2018/8/14.
+ */
+@Data
+public class Face {
+
+    private String fileAURl;
+
+    private String fileBURL;
+
+    private float num;
+
+    private String maeeage;
+
+    private String isPass;
+}
