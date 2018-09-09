595e79c376ea7589dbae9658c54697f2338f8fc4
cuiyang <1010985950@qq.com>
2018-09-09 17:56:48 +0800

    until修改

--- src/main/java/com/dhht/service/seal/Impl/SealServiceImpl.java
+++ src/main/java/com/dhht/service/seal/Impl/SealServiceImpl.java
@@ -298,7 +298,6 @@ public class SealServiceImpl implements SealService {
                     ImageIO.write(image,"bmp",new File(localPath));
                     File file = new File(localPath);
                     InputStream inputStream = new FileInputStream(file);
-                    ImageUtil.handleDpi(file,600,600);//设置dpi
                     byte[] imageData = FileUtil.readInputStream(inputStream);
                     FileInfo fileInfo = fileService.save(imageData, DateUtil.getCurrentTime() + seal.getUseDepartmentName() + sealType, "bmp", "", FileService.CREATE_TYPE_UPLOAD, user.getId(), user.getUserName());
                     String moulageImageId = fileInfo.getId();
