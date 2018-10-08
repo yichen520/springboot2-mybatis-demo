cc484675d609454fcc76438786e9f8f10b621531
XuZhengPing <591979486@qq.com>
2018-10-08 16:58:06 +0800

    芯片管理模块优化

--- src/main/java/com/dhht/model/ChipGrant.java
+++ src/main/java/com/dhht/model/ChipGrant.java
@@ -1,7 +1,9 @@
 package com.dhht.model;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class ChipGrant {
 
     private String id;
@@ -13,6 +15,8 @@ public class ChipGrant {
     private String chipCodeEnd;
 
     private String receiver;
+
+    private String receiverTel;
 
     private Integer grantNum;
 
@@ -28,99 +32,7 @@ public class ChipGrant {
 
     private Date recordTime;
 
-    public String getId() {
-        return id;
-    }
+    private String districtId;
 
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
 
-    public String getChipApplyId() {
-        return chipApplyId;
-    }
-
-    public void setChipApplyId(String chipApplyId) {
-        this.chipApplyId = chipApplyId == null ? null : chipApplyId.trim();
-    }
-
-    public String getChipCodeStart() {
-        return chipCodeStart;
-    }
-
-    public void setChipCodeStart(String chipCodeStart) {
-        this.chipCodeStart = chipCodeStart == null ? null : chipCodeStart.trim();
-    }
-
-    public String getChipCodeEnd() {
-        return chipCodeEnd;
-    }
-
-    public void setChipCodeEnd(String chipCodeEnd) {
-        this.chipCodeEnd = chipCodeEnd == null ? null : chipCodeEnd.trim();
-    }
-
-    public String getReceiver() {
-        return receiver;
-    }
-
-    public void setReceiver(String receiver) {
-        this.receiver = receiver == null ? null : receiver.trim();
-    }
-
-    public Integer getGrantNum() {
-        return grantNum;
-    }
-
-    public void setGrantNum(Integer grantNum) {
-        this.grantNum = grantNum;
-    }
-
-    public Date getGrantTime() {
-        return grantTime;
-    }
-
-    public void setGrantTime(Date grantTime) {
-        this.grantTime = grantTime;
-    }
-
-    public String getGranterId() {
-        return granterId;
-    }
-
-    public void setGranterId(String granterId) {
-        this.granterId = granterId == null ? null : granterId.trim();
-    }
-
-    public String getGranter() {
-        return granter;
-    }
-
-    public void setGranter(String granter) {
-        this.granter = granter == null ? null : granter.trim();
-    }
-
-    public String getGrantWay() {
-        return grantWay;
-    }
-
-    public void setGrantWay(String grantWay) {
-        this.grantWay = grantWay == null ? null : grantWay.trim();
-    }
-
-    public String getMemo() {
-        return memo;
-    }
-
-    public void setMemo(String memo) {
-        this.memo = memo == null ? null : memo.trim();
-    }
-
-    public Date getRecordTime() {
-        return recordTime;
-    }
-
-    public void setRecordTime(Date recordTime) {
-        this.recordTime = recordTime;
-    }
 }
