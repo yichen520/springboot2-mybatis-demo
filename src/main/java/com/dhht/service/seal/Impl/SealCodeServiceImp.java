dd13a38e17a55688748417bfdd3ccad58246224e
FYC <847667384@qq.com>
2018-10-10 17:26:10 +0800

    查询模块从业人员详情

---
+++ src/main/java/com/dhht/service/seal/Impl/SealCodeServiceImp.java
@@ -0,0 +1,39 @@
+package com.dhht.service.seal.Impl;
+
+import com.dhht.dao.SealDao;
+import com.dhht.model.Seal;
+import com.dhht.model.SealCode;
+import com.dhht.service.seal.SealCodeService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SealCodeServiceImp implements  SealCodeService {
+
+    @Autowired
+    private SealDao sealDao;
+
+
+    /**
+     * 获取备案单位编号
+     * @return
+     */
+    @Override
+    public List<SealCode> selectRecordDepartmentCode() {
+        return sealDao.selectRecordDepartmentCode();
+    }
+
+    /**
+     * 获取印章编号
+     * @param code
+     * @return
+     */
+    @Override
+    public String selectSealCode(String code) {
+        return sealDao.selectSealCode(code);
+    }
+}
