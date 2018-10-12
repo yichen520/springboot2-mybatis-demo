b4dea36381d9171a908aa4657ed39e1445ea8f53
cuiyang <1010985950@qq.com>
2018-10-12 10:24:18 +0800

    国庆后

--- src/test/java/com/dhht/ApplicationTests.java
+++ src/test/java/com/dhht/ApplicationTests.java
@@ -1,29 +1,29 @@
-package com.dhht;
-
-import com.dhht.model.SealCode;
-import com.dhht.service.seal.SealCodeService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTests {
-
-    @Autowired
-    private SealCodeService sealCodeService;
-
-    @Test
-    public void contextLoads(){
-        List<SealCode> sealCodes = sealCodeService.info("330000");
-        for (SealCode sealCode:sealCodes) {
-            System.out.println(sealCode.toString());
-        }
-    }
-
-}
+//package com.dhht;
+//
+//import com.dhht.model.SealCode;
+//import com.dhht.service.seal.SealCodeService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringBootConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.List;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ApplicationTests {
+//
+//    @Autowired
+//    private SealCodeService sealCodeService;
+//
+//    @Test
+//    public void contextLoads(){
+//        List<SealCode> sealCodes = sealCodeService.info("330000");
+//        for (SealCode sealCode:sealCodes) {
+//            System.out.println(sealCode.toString());
+//        }
+//    }
+//
+//}
