1a6b6702d286f920022d5ba7ee05df300f755ab4
cuiyang <1010985950@qq.com>
2019-01-15 14:41:42 +0800

    log 问题

--- src/main/java/com/dhht/controller/wechat/SuspiciousWeChatController.java
+++ src/main/java/com/dhht/controller/wechat/SuspiciousWeChatController.java
@@ -1,9 +1,18 @@
 package com.dhht.controller.wechat;
 
+import com.dhht.annotation.Log;
+import com.dhht.common.JsonObjectBO;
+import com.dhht.model.Suspicious;
+import com.dhht.model.User;
 import com.dhht.service.suspicious.SuspiciousService;
+import com.dhht.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/weChat/suspicious")
@@ -12,5 +21,20 @@ public class SuspiciousWeChatController {
     @Autowired
     private SuspiciousService suspiciousService;
 
-
+    /**
+     * 可疑情况添加
+     * @param
+     * @return
+     */
+//    @Log("可疑情况添加")
+//    @RequestMapping(value = "/add")
+//    public JsonObjectBO add(@RequestBody Suspicious suspicious, HttpServletRequest httpServletRequest) {
+//        User user = (User) httpServletRequest.getSession().getAttribute("user");
+//        try {
+//            return ResultUtil.getResult(suspiciousService.insertsuspicious(suspicious, user));
+//        } catch (Exception e) {
+//            logger.error(e.getMessage(),e);
+//            return JsonObjectBO.exception("添加失败！");
+//        }
+//    }
 }
